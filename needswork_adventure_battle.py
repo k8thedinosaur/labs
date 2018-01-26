e3fd8b17d0603229cbd90d8ff3be42ce8d7e8f04
Kate <never.say.dinosaur@gmail.com>
2018-01-25 16:56:01 -0800

    refactored, improved, DRY'd adventure battle.

--- needswork_adventure_battle.py
+++ needswork_adventure_battle.py
@@ -1,5 +1,5 @@
 # some things to let's do:
-# figure out "break outside loop" issue so repeated actions can be made into smaller functions and reduce verbosity of text in program (unlike right here)
+# how to get baddie to not fight
 
 # implement magic system
 #     assign magic points to character which deplete when spells are used
@@ -14,16 +14,19 @@
 
 # allow enemy to take random actions (fight, magic, restore)
 
+# pop escape tactics from list after use
+#     message stating "no escape tactics left"
+
 
 import random
 from random import randint
 from needswork_adventure import AnHero, BadGuy, Weapon, Spell, Restorative
 
 # Weapons
-pointy_stick = Weapon("Mr. Pointy Stick", 4, "a tree")
-better_dagger = Weapon("A Better Dagger", 5, "the ground")
-incred_sword = Weapon("Sword of Incredibly Many Stabbings", 10, "between the ribs of a spooky skeleton")
-found_weapons = [pointy_stick, better_dagger, incred_sword]
+pointy_stick = Weapon("Mr. Pointy Stick", 4, "on a tree")
+better_dagger = Weapon("A Better Dagger", 5, "on the ground")
+incredible_sword = Weapon("Sword of Incredibly Many Stabbings", 10, "between the ribs of a spooky skeleton")
+found_weapons = [pointy_stick, better_dagger, incredible_sword]
 
 # Restoratives
 donut = Restorative("Box of Donuts", 5)
@@ -43,33 +46,27 @@ some_jerk.health = 25
 attack_sp = Spell("Attack", 3, 6)
 cure_sp = Spell("Cure", 5, 10)
 
-# how to get break to work?
-# error: "break outside loop"
-# def is_dead():
-#         if an_hero.health <= 0:
-#             print("{} has deathed.".format(an_hero.name))
-#             print("Oh no.")
-#             break
-#
-#         if some_jerk.health <= 0:
-#             print("{} has deathed.".format(some_jerk.name))
-#             print("You win the battle!")
-#             break
+diversions = ["{0} tells a 'your mom' joke too filthy to repeat here, but {1} was spawned from the "
+                      "depths of hell and has no mother, so {0}'s taunt has no effect.".format(an_hero.name,
+                                                                                               some_jerk.name),
+                      "{0} tries to distract {1} with a wholesome magic trick, but {0} eats {1} props and "
+                      "kicks {0} in the shins for good measure.".format(an_hero.name, some_jerk.name)]
 
-# same issue
-# def escape_attempt():
-#     escape = randint(0, 3)
-#     if escape >= 3:
-#         print("Your escape is successful! You run away like a little baby.")
-#         break
-#     # if unsuccessful:
-#     else:
-#         diversions = ["You tell a 'your mom' joke too filthy to repeat here, but {} was spawned from the "
-#                       "depths of hell and has no mother, so your taunt has no effect.".format(some_jerk.name),
-#                       "You try to distract {0} with a wholesome magic trick, but {0} eats your props and "
-#                       "kicks you in the shins for good measure.".format(some_jerk.name)]
-#         print(random.choice(diversions))
-#         print("Escape unsuccessful!")
+
+def escape_attempt():
+    escape = randint(0, 2)  # set to 2 for testing diversions; set to 3 to operate properly
+    if escape >= 3:
+        print("Your escape is successful! You run away like a little baby.")
+        exit()
+    # if unsuccessful:
+    else:
+        # print(diversions)
+        print(random.choice(diversions))
+        diversions.pop()
+        # print(diversions)
+        print("Escape unsuccessful!")
+    health_below_zero()
+
 
 def hero_magic_attack():
     spell_success = randint(0, 1)
@@ -77,11 +74,13 @@ def hero_magic_attack():
         hero_magic_power = randint(1, attack_sp.power)
         return hero_magic_power
 
+
 def hero_magic_cure():
     spell_success = randint(0, 1)
     if spell_success == 1:
         hero_magic_restore = randint(1, cure_sp.power)
         return hero_magic_restore
+
 
 def hero_magic():
     print("Which spell? \n\
@@ -120,20 +119,14 @@ def hero_magic():
             print("{0} doesn't have enough magic! ({0} has {1} points.)".format(an_hero.name, an_hero.magic))
     else:
         fumble()
+    health_below_zero()
 
 # def baddie_magic():
 # copy contents
 
-def hero_attack():
-    hero_attack_power = randint(0, an_hero.weapon.attack)
-    return hero_attack_power
-
-def baddie_attack():
-    baddie_attack_power = randint(0, some_jerk.weapon.attack)
-    return baddie_attack_power
 
 def hero_fight():
-    hero_attack_power = hero_attack()
+    hero_attack_power = randint(0, an_hero.weapon.attack)
     if hero_attack_power > 0:
         print("{} attacks {} with {} for {} damage!".format(an_hero.name, some_jerk.name,
                                                             an_hero.weapon, hero_attack_power))
@@ -141,8 +134,9 @@ def hero_fight():
     else:
         print("{0} attacks {1} with {2}. It's a miss! {0} burns with shame.".format(an_hero.name, some_jerk.name, an_hero.weapon))
 
+
 def baddie_fight():
-    baddie_attack_power = baddie_attack()
+    baddie_attack_power = randint(0, some_jerk.weapon.attack)
     if baddie_attack_power > 0:
         print("{} attacks {} with {} for {} damage!".format(some_jerk.name, an_hero.name,
                                                             some_jerk.weapon, baddie_attack_power))
@@ -150,32 +144,75 @@ def baddie_fight():
     else:
         print("{0} attacks {1} with {2}. It's a miss! {0} sulks in the corner.".format(some_jerk.name, an_hero.name,  some_jerk.weapon))
 
-# def restore(donut):
-#     for i in an_hero.inventory:
-#         if i == donut.name:
-#             print("{} uses {} to gain {} healths!".format(an_hero.name, donut.name, donut.restore))
-#             print(an_hero.inventory)
-#             an_hero.inventory.pop(donut.name)
-#             print(an_hero.inventory)
-#             an_hero.health += donut.restore
-#             print("{}'s health is {}.".format(an_hero.name, an_hero.health))
-#             print("-------------------------")
-#         else:
-#             print("{} doesn't have any items!".format(an_hero.name))
+
+# testing, not working as planned
+def restore():
+    for i in an_hero.inventory:
+        if i == donut.name:
+            print("{} uses {} to gain {} healths!".format(an_hero.name, donut.name, donut.restore))
+            print(an_hero.inventory)
+            an_hero.inventory.pop(0)
+            print(an_hero.inventory)
+            an_hero.health += donut.restore
+            print("{}'s health is {}.".format(an_hero.name, an_hero.health))
+            print("-------------------------")
+        else:
+            print("{} doesn't have any items!".format(an_hero.name))
+
+
+def search_for_weapon():
+    new_weapon = randint(1, 5)
+    if new_weapon >= 5:
+        which_weapon = random.choice(found_weapons)
+        an_hero.weapon = which_weapon
+        print("{} found {}. Sweet!".format(an_hero.name, which_weapon))
+
+    else:
+        print("{} didn't find anything.".format(an_hero.name))
+
 
 def fumble():
     print("That's not an option! While {} is fumbling around like a doofus...".format(an_hero.name))
-    baddie_fight()
+
+
+def more_info():
+    print("{} is using {}, maximum damage {}.".format(an_hero.name, an_hero.weapon, an_hero.weapon.attack))
+    print("{} is using {}, maximum damage {}.".format(some_jerk.name, some_jerk.weapon, some_jerk.weapon.attack))
+    print("-------------------------")
+    print("{}'s health is {}, magic is {}.".format(an_hero.name, an_hero.health, an_hero.magic))
+    print("{}'s health is {}, magic is none.".format(some_jerk.name, some_jerk.health))
+    print("-------------------------")
+
+
+def health_stats():
+    health_below_zero()
+    print("-------------------------")
+    print("{}'s health is {}.".format(an_hero.name, an_hero.health))
+    print("{}'s health is {}.".format(some_jerk.name, some_jerk.health))
+    print("-------------------------")
+
+
+def health_below_zero():
+    if an_hero.health <= 0:
+        an_hero.health = 0
+
+    if some_jerk.health <= 0:
+        some_jerk.health = 0
+
 
 def is_deathed():
     if an_hero.health <= 0:
+        an_hero.health = 0
         print("{} has deathed.".format(an_hero.name))
         print("Oh no.")
+        print("-------------------------")
         exit()
 
     if some_jerk.health <= 0:
+        some_jerk.health = 0
         print("{} has deathed.".format(some_jerk.name))
         print("You win the battle!")
+        print("-------------------------")
         exit()
 
 
@@ -199,79 +236,49 @@ def battle():
 
         # info
         if choice == "0":
-            print("{} is using {}, maximum damage {}.".format(an_hero.name, an_hero.weapon, an_hero.weapon.attack))
-            print("{} is using {}, maximum damage {}.".format(some_jerk.name, some_jerk.weapon, some_jerk.weapon.attack))
-            print("-------------------------")
-
-            print("{}'s health is {}, magic is {}.".format(an_hero.name, an_hero.health, an_hero.magic))
-            print("{}'s health is {}, magic is none.".format(some_jerk.name, some_jerk.health))
-            print("-------------------------")
+            more_info()
 
         # fight
         elif choice == "1":
             hero_fight()
             baddie_fight()
-            print("{}'s health is {}.".format(an_hero.name, an_hero.health, an_hero.magic))
-            print("{}'s health is {}.".format(some_jerk.name, some_jerk.health))
-            print("-------------------------")
+            health_stats()
             is_deathed()
 
+        # magic
         elif choice == "2":
             hero_magic()
             baddie_fight()
-
-            print("{}'s health is {}.".format(an_hero.name, an_hero.health, an_hero.magic))
-            print("{}'s health is {}.".format(some_jerk.name, some_jerk.health))
-            print("-------------------------")
+            health_stats()
             is_deathed()
 
         # restorative (broken)
         elif choice == "3":
-            restore(donut)
+            restore()
             baddie_fight()
+            is_deathed()
+            health_stats()
 
         # escape
         elif choice == "4":
-            escape = randint(0, 3)
-            if escape >= 3:
-                print("{0}'s escape is successful! {0} runs away like a little baby.".format(an_hero.name))
-                break
-            # if unsuccessful:
-            else:
-                diversions = ["{0} tells a 'your mom' joke too filthy to repeat here, but {1} was spawned from the "
-                              "depths of hell and has no mother, so {0} taunt has no effect.".format(an_hero.name, some_jerk.name),
-                              "{0} tries to distract {1} with a wholesome magic trick, but {1} eats {0}'s props and "
-                              "kicks {0} in the shins for good measure.".format(an_hero.name, some_jerk.name)]
-                print(random.choice(diversions))
-                print("Escape unsuccessful!")
-                baddie_fight()
-
-                print("{}'s health is {}.".format(an_hero.name, an_hero.health))
-                print("{}'s health is {}.".format(some_jerk.name, some_jerk.health))
-                print("-------------------------")
+            escape_attempt()
+            baddie_fight()
+            health_stats()
+            is_deathed()
 
         # search for weapon
         elif choice == "5":
-            new_weapon = randint(1, 5)
-            if new_weapon >= 5:
-                which_weapon = random.choice(found_weapons)
-                an_hero.weapon = which_weapon
-                print("{} found {}. Sweet!".format(an_hero.name, which_weapon))
-
-            else:
-                print("{} didn't find anything.".format(an_hero.name))
-
-                baddie_fight()
-                print("{}'s health is {}.".format(an_hero.name, an_hero.health))
-                print("{}'s health is {}.".format(some_jerk.name, some_jerk.health))
-                print("-------------------------")
+            search_for_weapon()
+            baddie_fight()
+            health_stats()
+            is_deathed()
 
         # fumble
         else:
             fumble()
-            print("{}'s health is {}.".format(an_hero.name, an_hero.health))
-            print("{}'s health is {}.".format(some_jerk.name, some_jerk.health))
-            print("-------------------------")
+            baddie_fight()
+            health_stats()
+            is_deathed()
 
 
 battle()
